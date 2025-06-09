ec5d3287afa82455ddddec912ba697b25aa5e90d
Varshana01 <91725838+Varshana01@users.noreply.github.com>
2025-06-09 21:20:00 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function playSound() {
   document.getElementById("celebration").play();
   const results = [
     "Detected brainwaves forming haikus in Morse code.",
-    "Subconscious ran a full Dungeons & Dragons campaign overnight.",
-    "Thinks in interpretive dance.",
-    "Brain emits mild jazz tones under pressure.",
-    "Can recite digits of pi backward in dreams."
+    "Subconscious ran a full Apex campaign overnight.",
+    "Thinks in stupid language.",
+    "Ventre faire gulu gulu under pressure.",
+    "Can recite nothing in dreams."
   ];
   const randomResult = results[Math.floor(Math.random() * results.length)];
   document.getElementById("random-result").innerText = randomResult;
